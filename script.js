c6b08ba8e1ba25bad6a31b4e38b5137710ea8204
Habib Khan <158816354+habibkhan004@users.noreply.github.com>
2024-03-25 01:22:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Defining Constants and variables
 let inputDir = { x: 0, y: 0 }
-let foodSound = new Audio('../music/food.mp3')
-let gameOver = new Audio("../music/gameover.mp3")
-let moveSound = new Audio("../music/move.mp3")
-let music = new Audio("../music/music.mp3")
+let foodSound = new Audio('music/food.mp3')
+let gameOver = new Audio("music/gameover.mp3")
+let moveSound = new Audio("music/move.mp3")
+let music = new Audio("music/music.mp3")
 let board = document.getElementById('game-board')
 let scoreBox = document.getElementById('score-box');
 let highScoreBox = document.getElementById('hiscore-box');
